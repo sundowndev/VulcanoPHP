0bdc6303fb0e2b49bc1dcfd4fdab644e241d0fac
sundowndev <anonhq789@protonmail.com>
2017-11-30 12:27:14 +0100

    Delete old logout model

--- app/Resources/models/admin/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if($app->getModule('Session\Session')->r('auth') === true){
-    $app->getModule('Session\Session')->destroy();
-    $app->redirect($app->config['paths']['admin']);
-} else {
-    // not connected
-}
