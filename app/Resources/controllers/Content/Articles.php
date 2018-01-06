6d0aad2f19b235a25dafb258f065a3817bbb0ae8
Raphael <SundownDEV@users.noreply.github.com>
2018-01-06 21:16:08 +0100

    Update Articles.php

--- app/Resources/controllers/Content/Articles.php
+++ app/Resources/controllers/Content/Articles.php
@@ -4,4 +4,12 @@ namespace Controllers\Content;
 class Articles
 { 
     public function __construct () {}
+    
+    public function createArticle ($article) {}
+    
+    public function editArticle ($id, $article) {}
+    
+    public function deleteArticle ($id) {}
+    
+    public function getArticle ($id) {}
 }
