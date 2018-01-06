b36918eb44b78a9c489ff1c12eb5cb4d6a66f03d
Raphael <SundownDEV@users.noreply.github.com>
2018-01-06 21:18:43 +0100

    Create CategoryModel.php

---
+++ app/Resources/controllers/Content/CategoryModel.php
@@ -0,0 +1,17 @@
+<?php
+namespace Controllers\Content;
+
+class CategoryModel
+{ 
+    public function __construct () {}
+    
+    public function createCategory ($article) {}
+    
+    public function editCategory ($id, $article) {}
+    
+    public function deleteCategory ($id) {}
+    
+    public function getCategory ($id) {}
+    
+    public function getAllCategories () {}
+}
