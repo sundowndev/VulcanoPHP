85f6c127c3a319cd8f02446bcecbf4e63120d495
SundownDEV <raphael@crvx.fr>
2018-04-14 21:43:53 +0200

    :art: Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,26 @@
 var Encore = require('@symfony/webpack-encore');
 
+const Theme = "default";
+
 Encore
     // the project directory where compiled assets will be stored
-    .setOutputPath('public/build/')
+    .setOutputPath('public/assets/')
     // the public path used by the web server to access the previous directory
-    .setPublicPath('/build')
+    .setPublicPath('/assets')
     .cleanupOutputBeforeBuild()
     .enableSourceMaps(!Encore.isProduction())
     // uncomment to create hashed filenames (e.g. app.abc123.css)
     // .enableVersioning(Encore.isProduction())
 
     // uncomment to define the assets of the project
-    // .addEntry('js/app', './assets/js/app.js')
-    // .addStyleEntry('css/app', './assets/css/app.scss')
+    .addEntry('js/app', './assets/' + Theme + '/js/app.js')
+    .addStyleEntry('css/app', './assets/' + Theme + '/scss/app.scss')
 
     // uncomment if you use Sass/SCSS files
-    // .enableSassLoader()
+    .enableSassLoader()
 
     // uncomment for legacy applications that require $/jQuery as a global variable
-    // .autoProvidejQuery()
+    .autoProvidejQuery()
 ;
 
 module.exports = Encore.getWebpackConfig();
