63f48c42515a5eda3950631ef7a4c6a3f48b6a32
SundownDEV <raphael@crvx.fr>
2018-04-21 12:18:38 +0200

    :zap: Controllers init

--- src/Controller/FrontOffice/DefaultController.php
+++ src/Controller/FrontOffice/DefaultController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\FrontOffice;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
