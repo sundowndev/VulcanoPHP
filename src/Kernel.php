5b55684dd6a9567c2e5ee1c6c817a10e4c3400fc
SundownDEV <raphael@crvx.fr>
2018-04-23 22:45:03 +0200

    :fire: Delete ignored log folder

--- src/Kernel.php
+++ src/Kernel.php
@@ -18,7 +18,7 @@ class Kernel extends BaseKernel
 
     public function getCacheDir()
     {
-        return $this->getProjectDir().'/'.self::APP_DIR.'/var/cache/'.$this->environment;
+        return $this->getProjectDir().'/var/cache/'.$this->environment;
     }
 
     public function getLogDir()
